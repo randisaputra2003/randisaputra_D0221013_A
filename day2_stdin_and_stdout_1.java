4bb436778aebaeb3b75559e01d06386fabc62c51
randisaputra2003 <https://github.com/randisaputra2003/randisaputra_D0221013_A.git>
2022-02-19 23:07:57 -0800

    initial commit

---
+++ day2_stdin_and_stdout_1.java
@@ -0,0 +1,16 @@
+package com.pemula;
+
+import java.util.*;
+
+public class day2_stdin_and_stdout_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
